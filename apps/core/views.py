e7a92f2f1308d789804b198c4aac4863af777ba1
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2017-11-25 13:56:32 -0200

    require login to see dashboard

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render, get_list_or_404
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_list_or_404, render
+
 from apps.tickets import models
 
 
+@login_required
 def index(request):
     tickets = get_list_or_404(models.Ticket)  # , active=True)
     return render(request, 'core/index.html', {'tickets': tickets})
