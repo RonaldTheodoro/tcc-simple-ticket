f25da98dfe6191a88a25f778ff0a331e42d96b15
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2018-03-21 18:40:15 -0300

    Remove email config

--- config/settings.py
+++ config/settings.py
@@ -129,13 +129,3 @@ LOGIN_URL = 'accounts:login'
 LOGOUT_URL = 'accounts:logout'
 LOGIN_REDIRECT_URL = 'core:index'
 LOGOUT_REDIRECT_URL = 'core:index'
-
-# Email config
-
-EMAIL_BACKEND = config('EMAIL_BACKEND')
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT', cast=int)
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
-# EMAIL_USE_SSL = config('EMAIL_USE_SSL', default=True, cast=bool)
