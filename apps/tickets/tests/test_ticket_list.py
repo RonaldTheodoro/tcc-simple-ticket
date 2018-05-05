3fcbee137131feb477e052fa09377bf0ce67fb35
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2018-05-05 10:40:43 -0300

    add TestTicketList

--- apps/tickets/tests/test_ticket_list.py
+++ apps/tickets/tests/test_ticket_list.py
@@ -4,18 +4,19 @@ from django.urls import reverse
 from apps.tickets.models import User
 
 
-class HomeTest(TestCase):
+class TestTicketList(TestCase):
 
     def setUp(self):
         user_data = {'username': 'ronald', 'password': '123456'}
         User.objects.create_user(**user_data)
         self.client.login(**user_data)
-        self.response = self.client.get(reverse('core:index'), follow=True)
+        self.response = self.client.get(reverse('tickets:list'), follow=True)
 
     def test_get(self):
-        """GET / must return status code 200"""
+        """GET /tickets/ must return status code 200"""
         self.assertEqual(200, self.response.status_code)
 
     def test_template(self):
-        """Must use index.html"""
-        self.assertTemplateUsed(self.response, 'index.html')
+        """Must use list.html"""
+        self.assertTemplateUsed(self.response, 'list.html')
+
